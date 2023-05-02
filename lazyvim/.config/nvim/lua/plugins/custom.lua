dc745fe5f681d7fd6db4d9c80af1d92231eefc0a
Pagliacii <huangmianrui0310@outlook.com>
2023-05-02 20:56:06 +0800

    feat: add a new plugin for orgmode

--- lazyvim/.config/nvim/lua/plugins/custom.lua
+++ lazyvim/.config/nvim/lua/plugins/custom.lua
@@ -102,4 +102,14 @@ return {
     "christoomey/vim-tmux-navigator",
     event = "VeryLazy",
   },
+
+  {
+    "nvim-orgmode/orgmode",
+    ft = { "org" },
+    config = function()
+      local orgmode = require("orgmode")
+      orgmode.setup_ts_grammar()
+      orgmode.setup({})
+    end,
+  },
 }
