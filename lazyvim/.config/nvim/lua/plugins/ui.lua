cdc75098cebb6c61d6feefc15a9ae28e353ade0c
Pagliacii <huangmianrui0310@outlook.com>
2023-05-07 23:55:37 +0800

    feat: add my logo on dashboard

--- lazyvim/.config/nvim/lua/plugins/ui.lua
+++ lazyvim/.config/nvim/lua/plugins/ui.lua
@@ -17,4 +17,36 @@ return {
       winblend = 75,
     },
   },
+
+  {
+    "goolord/alpha-nvim",
+    opts = function(_, dashboard)
+      local logo = [[
+██████╗  █████╗  ██████╗ ██╗     ██╗ █████╗  ██████╗██╗██╗
+██╔══██╗██╔══██╗██╔════╝ ██║     ██║██╔══██╗██╔════╝██║██║
+██████╔╝███████║██║  ███╗██║     ██║███████║██║     ██║██║
+██╔═══╝ ██╔══██║██║   ██║██║     ██║██╔══██║██║     ██║██║
+██║     ██║  ██║╚██████╔╝███████╗██║██║  ██║╚██████╗██║██║
+╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝╚═╝
+                  [ github.com/Pagliacii ]
+      ]]
+      dashboard.section.header.val = vim.split(logo, "\n", {})
+      dashboard.config.opts.setup = function()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "AlphaReady",
+          desc = "disable tabline for alpha",
+          callback = function()
+            vim.opt.showtabline = 0
+          end,
+        })
+        vim.api.nvim_create_autocmd("BufUnload", {
+          buffer = 0,
+          desc = "enable tabline after alpha",
+          callback = function()
+            vim.opt.showtabline = 2
+          end,
+        })
+      end
+    end,
+  },
 }
