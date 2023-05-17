644b30cbf67b92020f5e79c7985f3184f592c060
Pagliacii <huangmianrui0310@outlook.com>
2023-05-18 01:22:28 +0800

    feat: use gopher.nvim to enhance golang coding

--- lazyvim/.config/nvim/lua/plugins/extras/lang/lua.lua
+++ lazyvim/.config/nvim/lua/plugins/extras/lang/lua.lua
@@ -31,4 +31,28 @@ return {
       })
     end,
   },
+
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = function(_, opts)
+      ---@type lspconfig.options
+      vim.list_extend(opts.servers, {
+        lua_ls = {
+          settings = {
+            Lua = {
+              runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+              },
+              diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
 }
