760deef941ab99482366342006ee9f46d68d71e7
Pagliacii <huangmianrui0310@outlook.com>
2023-07-18 00:00:13 +0800

    feat: upgrade plugins

--- lazyvim/.config/nvim/lua/plugins/editor.lua
+++ lazyvim/.config/nvim/lua/plugins/editor.lua
@@ -5,7 +5,9 @@ return {
     opts = {
       close_if_last_window = true,
       filesystem = {
-        follow_current_file = true,
+        follow_current_file = {
+          enabled = true,
+        },
         group_empty_dirs = true,
         hijack_netrw_behavior = "open_default",
         filtered_items = {
