00c191b2d683a1c3d6ae8d131eb6e0182f7469b6
Pagliacii <huangmianrui0310@outlook.com>
2023-09-09 23:06:42 +0800

    feat: introduce the hydrate.nvim plugin

--- lazyvim/.config/nvim/lua/plugins/util.lua
+++ lazyvim/.config/nvim/lua/plugins/util.lua
@@ -259,4 +259,30 @@ return {
       })
     end,
   },
+
+  {
+    "stefanlogue/hydrate.nvim",
+    lazy = false,
+    opts = {
+      -- The interval between notifications in minutes
+      minute_interval = 15,
+      -- The render style for notifications
+      -- Accepted values are "default", "minimal", "simple" or "compact"
+      render_style = "compact",
+      -- Loads time of last drink on startup
+      -- Useful if you don't have long-running neovim instances
+      -- or if you tend to have multiple instances running at a time
+      persist_timer = true,
+    },
+    cmd = {
+      "HydrateWhen",
+      "HydrateNow",
+      "DrinkInterval",
+      "HydrateDisable",
+      "HydrateEnable",
+    },
+    keys = {
+      { "<leader>h", "<cmd>HydrateNow<cr>", desc = "Drink water", noremap = true, silent = true },
+    },
+  },
 }
