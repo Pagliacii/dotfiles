c6fd04eaf90b69ae9abf65107f49fd7bab785143
Pagliacii <huangmianrui0310@outlook.com>
2024-09-18 00:15:13 +0800

    chore: remove old color schemes

--- lazyvim/.config/nvim/lua/plugins/colorscheme.lua
+++ lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -12,44 +12,6 @@ return {
   },
 
   {
-    "sainnhe/gruvbox-material",
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      vim.g.gruvbox_material_enable_italic = true
-      vim.g.gruvbox_material_enable_bold = true
-      -- Set contrast.
-      vim.g.gruvbox_material_background = "medium"
-      -- For better performance
-      vim.g.gruvbox_material_better_performance = true
-    end,
-  },
-
-  {
-    "rose-pine/neovim",
-    cond = false,
-    name = "rose-pine",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("rose-pine").setup({
-        dark_variant = "moon",
-      })
-    end,
-  },
-
-  {
-    "Mofiqul/vscode.nvim",
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    config = true,
-  },
-
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
