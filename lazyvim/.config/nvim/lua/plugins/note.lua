bffd6c04ab277305d103751b0030fc15c5282d3a
Pagliacii <huangmianrui0310@outlook.com>
2024-10-15 22:48:51 +0800

    chore(lazyvim): remove unused note plugins

--- lazyvim/.config/nvim/lua/plugins/note.lua
+++ lazyvim/.config/nvim/lua/plugins/note.lua
@@ -1,7 +1,3 @@
-local function quicknote_cmd(name)
-  return string.format("<cmd>lua require('quicknote').%s()<cr>", name)
-end
-
 local function toggle_venn()
   local venn_enabled = vim.inspect(vim.b.venn_enabled)
   if venn_enabled == "nil" then
@@ -25,65 +21,14 @@ end
 
 local prefix = "<leader>n"
 local neorg_prefix = prefix .. "o"
-local qn_prefix = prefix .. "q"
-local zt_prefix = prefix .. "z"
 local ob_prefix = prefix .. "b"
 
 return {
-  {
-    "RutaTang/quicknote.nvim",
-    config = true,
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-      { qn_prefix .. "lc", quicknote_cmd("NewNoteAtCurrentLine"), desc = "Create" },
-      { qn_prefix .. "lo", quicknote_cmd("OpenNoteAtCurrentLine"), desc = "Open" },
-      { qn_prefix .. "ld", quicknote_cmd("DeleteNoteAtCurrentLine"), desc = "Delete" },
-      { qn_prefix .. "wc", quicknote_cmd("NewNoteAtCWD"), desc = "Create" },
-      { qn_prefix .. "wo", quicknote_cmd("OpenNoteAtCWD"), desc = "Open" },
-      { qn_prefix .. "wd", quicknote_cmd("DeleteNoteAtCWD"), desc = "Delete" },
-      { qn_prefix .. "gc", quicknote_cmd("NewNoteAtGlobal"), desc = "Create" },
-      { qn_prefix .. "go", quicknote_cmd("OpenNoteAtGlobal"), desc = "Open" },
-      { qn_prefix .. "gd", quicknote_cmd("DeleteNoteAtGlobal"), desc = "Delete" },
-      { qn_prefix .. "Lb", quicknote_cmd("ListNotesForCurrentBuffer"), desc = "Current buffer" },
-      { qn_prefix .. "Lc", quicknote_cmd("ListNotesForCWD"), desc = "CWD" },
-      { qn_prefix .. "Lg", quicknote_cmd("ListNotesForGlobal"), desc = "Global" },
-      { qn_prefix .. "Lf", quicknote_cmd("ListNotesForAFileOrWDInCWD"), desc = "File or dir" },
-      { qn_prefix .. "jp", quicknote_cmd("JumpToPreviousNote"), desc = "Previous" },
-      { qn_prefix .. "jn", quicknote_cmd("JumpToNextNote"), desc = "Next" },
-      { qn_prefix .. "cb", quicknote_cmd("GetNotesCountForCurrentBuffer"), desc = "Current buffer" },
-      { qn_prefix .. "cc", quicknote_cmd("GetNotesCountForCWD"), desc = "CWD" },
-      { qn_prefix .. "cg", quicknote_cmd("GetNotesCountForGlobal"), desc = "Global" },
-      { qn_prefix .. "s", quicknote_cmd("ToggleNoteSigns"), desc = "Toggle signs" },
-      { qn_prefix .. "eb", quicknote_cmd("ExportNotesForCurrentBuffer"), desc = "Current buffer" },
-      { qn_prefix .. "ec", quicknote_cmd("ExportNotesForCWD"), desc = "CWD" },
-      { qn_prefix .. "eg", quicknote_cmd("ExportNotesForGlobal"), desc = "Global" },
-      { qn_prefix .. "ib", quicknote_cmd("ImportNotesForCurrentBuffer"), desc = "Current buffer" },
-      { qn_prefix .. "ic", quicknote_cmd("ImportNotesForCWD"), desc = "CWD" },
-      { qn_prefix .. "ig", quicknote_cmd("ImportNotesForGlobal"), desc = "Global" },
-    },
-  },
-
   {
     "jbyuki/venn.nvim",
     cmd = { "VBox" },
     keys = {
       { prefix .. "v", toggle_venn, desc = "Venn", noremap = true },
-    },
-  },
-
-  {
-    "backdround/global-note.nvim",
-    cmd = { "GlobalNote" },
-    config = true,
-    keys = {
-      {
-        prefix .. "g",
-        function()
-          require("global-note").toggle_note()
-        end,
-        desc = "Global Note",
-        noremap = true,
-      },
     },
   },
 
@@ -204,76 +149,6 @@ return {
       { neorg_prefix .. "r", "<cmd>Neorg roam<cr>", desc = "Roam" },
       { neorg_prefix .. "t", "<cmd>Neorg cycle_task<cr>", desc = "Cycle task" },
     },
-  },
-
-  {
-    "nvim-orgmode/orgmode",
-    event = "BufRead",
-    ft = { "org" },
-    opts = {
-      org_agenda_files = "~/orgfiles/**/*",
-      org_default_notes_file = "~/orgfiles/refile.org",
-    },
-  },
-
-  {
-    "renerocksai/telekasten.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "nvim-telekasten/calendar-vim",
-    },
-    cmd = { "Telekasten" },
-    opts = {
-      home = vim.fn.expand("~/zettelkasten"),
-    },
-    keys = {
-      { "<localleader>z", "<cmd>Telekasten panel<cr>", desc = "Panel", noremap = true },
-      { zt_prefix .. "p", "<cmd>Telekasten panel<cr>", desc = "Panel", noremap = true },
-      { zt_prefix .. "f", "<cmd>Telekasten find_notes<cr>", desc = "Find notes", noremap = true },
-      { zt_prefix .. "F", "<cmd>Telekasten find_friends<cr>", desc = "Find friends", noremap = true },
-      { zt_prefix .. "g", "<cmd>Telekasten search_notes<cr>", desc = "Search notes", noremap = true },
-      { zt_prefix .. "d", "<cmd>Telekasten goto_today<cr>", desc = "Daily notes", noremap = true },
-      { zt_prefix .. "D", "<cmd>Telekasten find_daily_notes<cr>", desc = "Find daily notes", noremap = true },
-      { zt_prefix .. "w", "<cmd>Telekasten goto_thisweek<cr>", desc = "Weekly notes", noremap = true },
-      { zt_prefix .. "W", "<cmd>Telekasten find_weekly_notes<cr>", desc = "Find weekly notes", noremap = true },
-      { zt_prefix .. "z", "<cmd>Telekasten follow_link<cr>", desc = "Follow link", noremap = true },
-      { zt_prefix .. "n", "<cmd>Telekasten new_note<cr>", desc = "New note", noremap = true },
-      { zt_prefix .. "N", "<cmd>Telekasten new_templated_note<cr>", desc = "New note by template", noremap = true },
-      { zt_prefix .. "c", "<cmd>Telekasten show_calendar<cr>", desc = "Calendar", noremap = true },
-      { zt_prefix .. "b", "<cmd>Telekasten show_backlinks<cr>", desc = "Backlinks", noremap = true },
-      { zt_prefix .. "t", "<cmd>Telekasten toggle_todo<cr>", desc = "Toggle todo", noremap = true },
-      { zt_prefix .. "T", "<cmd>Telekasten show_tags<cr>", desc = "Tags", noremap = true },
-      { zt_prefix .. "I", "<cmd>Telekasten insert_img_link<cr>", desc = "Insert image link", noremap = true },
-      { "[[", "<cmd>Telekasten insert_link<cr>", mode = "i", desc = "Insert image link", noremap = true },
-      { zt_prefix .. "p", "<cmd>Telekasten paste_img_and_link<cr>", desc = "Paste image and link", noremap = true },
-      -- { zt_prefix .. "P", "<cmd>Telekasten preview_img<cr>", desc = "Preview image", noremap = true },
-      { zt_prefix .. "y", "<cmd>Telekasten yank_notelink<cr>", desc = "Yank note link", noremap = true },
-      -- { zt_prefix .. "B", "<cmd>Telekasten browse_media<cr>", desc = "Browse media files", noremap = true },
-      { zt_prefix .. "r", "<cmd>Telekasten rename_note<cr>", desc = "Rename note", noremap = true },
-      { zt_prefix .. "v", "<cmd>Telekasten switch_vault<cr>", desc = "Switch vault", noremap = true },
-    },
-  },
-
-  {
-    "JellyApple102/flote.nvim",
-    config = true,
-    cmd = "Flote",
-    keys = {
-      { prefix .. "f", "<cmd>Flote<cr>", desc = "Flote", noremap = true },
-    },
-  },
-
-  {
-    "zk-org/zk-nvim",
-    enabled = vim.fn.executable("zk") == 1,
-    config = function()
-      require("zk").setup({
-        -- can be "telescope", "fzf", "fzf_lua", "minipick", or "select" (`vim.ui.select`)
-        -- it's recommended to use "telescope", "fzf", "fzf_lua", or "minipick"
-        picker = "telescope",
-      })
-    end,
-    cmd = { "ZkNew", "ZkNotes" },
   },
 
   {
