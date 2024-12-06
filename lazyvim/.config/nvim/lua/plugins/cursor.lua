66bb1d7464b7e75886e194cfff151deac9fb0756
Pagliacii <huangmianrui0310@outlook.com>
2024-12-06 23:48:26 +0800

    chore(modes.nvim): add ignore filetypes

--- lazyvim/.config/nvim/lua/plugins/cursor.lua
+++ lazyvim/.config/nvim/lua/plugins/cursor.lua
@@ -28,6 +28,9 @@ return {
       set_cursor = false,
       -- Enable line number highlights to match cursonline
       set_number = false,
+      -- Disable modes highlights in specified filetypes
+      -- Please PR commonly ignored filetypes
+      ignore_filetypes = { "NvimTree", "TelescopePrompt", "noice", "glow", "alpha", "dashboard" },
     },
   },
 }
