a6c72c4cbc336af4f77c2f04ea1fc23a07c097b2
Pagliacii <huangmianrui0310@outlook.com>
2024-12-12 00:03:03 +0800

    feat(lazyvim): add timber.nvim for debug logs

--- lazyvim/.config/nvim/lua/plugins/debug.lua
+++ lazyvim/.config/nvim/lua/plugins/debug.lua
@@ -223,4 +223,15 @@ return {
       },
     },
   },
+
+  {
+    "Goose97/timber.nvim",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("timber").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
 }
