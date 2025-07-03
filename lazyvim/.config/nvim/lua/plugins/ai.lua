fa3469ea47d07f18145af950f0fcad1458d17a5a
Pagliacii <huangmianrui0310@outlook.com>
2025-07-04 00:08:08 +0800

    feat: add VectorCode plugin for neovim
    - Add VectorCode plugin with semantic code search
    - Configure codecompanion integration for vectorcode tools
    - Update mason to install vectorcode binary
    - Fix yazi config section name from manager to mgr
    - Update lazy-lock with latest plugin versions

--- lazyvim/.config/nvim/lua/plugins/ai.lua
+++ lazyvim/.config/nvim/lua/plugins/ai.lua
@@ -296,6 +296,47 @@ return {
             show_result_in_chat = true,
           },
         },
+        vectorcode = {
+          enabled = vim.fn.executable("vectorcode") == 1,
+          ---@type VectorCode.CodeCompanion.extensionOpts
+          opts = {
+            tool_group = {
+              -- this will register a tool group called `@vectorcode_toolbox` that contains all 3 tools
+              enabled = true,
+              -- a list of extra tools that you want to include in `@vectorcode_toolbox`.
+              -- if you use @vectorcode_vectorise, it'll be very handy to include
+              -- `file_search` here.
+              extras = {},
+              collapse = false, -- whether the individual tools should be shown in the chat
+            },
+            tool_opts = {
+              ---@type VectorCode.CodeCompanion.ToolOpts
+              ["*"] = {},
+              ---@type VectorCode.CodeCompanion.LsToolOpts
+              ls = {},
+              ---@type VectorCode.CodeCompanion.VectoriseToolOpts
+              vectorise = {},
+              ---@type VectorCode.CodeCompanion.QueryToolOpts
+              query = {
+                max_num = { chunk = -1, document = -1 },
+                default_num = { chunk = 50, document = 10 },
+                include_stderr = false,
+                use_lsp = false,
+                no_duplicate = true,
+                chunk_mode = false,
+                ---@type VectorCode.CodeCompanion.SummariseOpts
+                summarise = {
+                  ---@type boolean|(fun(chat: CodeCompanion.Chat, results: VectorCode.QueryResult[]): boolean)|nil
+                  enabled = true,
+                  adapter = nil,
+                  query_augmented = true,
+                },
+              },
+              files_ls = {},
+              files_rm = {},
+            },
+          },
+        },
       },
     },
     dependencies = {
@@ -303,5 +344,23 @@ return {
       "nvim-treesitter/nvim-treesitter",
       "ravitemer/mcphub.nvim",
     },
+  },
+
+  {
+    "Davidyz/VectorCode",
+    version = "*",
+    enabled = not jit.os:find("Windows"),
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "mason-org/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, {
+            "vectorcode",
+          })
+        end,
+      },
+    },
+    cmd = "VectorCode",
   },
 }
