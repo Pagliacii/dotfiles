a712a506fb783c2eac63b71fd68a7a9b3aca952c
Pagliacii <huangmianrui0310@outlook.com>
2025-08-22 22:02:17 +0800

    chore: update LazyVim plugins and add Mercurial support to Starship
    - Added hg_branch module to Starship prompt with custom styling and truncation
    - Updated 30+ LazyVim plugins to latest versions including core packages like nvim-lspconfig, catppuccin, copilot.lua, and various development tools

--- lazyvim/.config/nvim/lua/plugins/colorscheme.lua
+++ lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -53,6 +53,16 @@ return {
   },
 
   {
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
+
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-frappe",
